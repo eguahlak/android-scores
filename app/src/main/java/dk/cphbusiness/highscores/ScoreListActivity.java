05ff910c2a7c83004a223a79a90179aa67133d62
Anders Kalhauge <anders@kalhauge.dk>
2015-11-16 10:30:26 +0100

    Score List activity added

---
+++ app/src/main/java/dk/cphbusiness/highscores/ScoreListActivity.java
@@ -0,0 +1,13 @@
+package dk.cphbusiness.highscores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_list);
+    }
+}
